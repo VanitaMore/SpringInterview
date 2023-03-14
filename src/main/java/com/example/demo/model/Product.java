7b33f4cfb48230aede95007a8c322d4d958f0fc2
Vanita More <Vanita.More@einfochips.com>
2023-03-14 13:56:32 +0530

    initial commit

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column
+	private String name;
+	private String description;
+
+	/*
+	 * private BigDecimal price;
+	 * 
+	 * @CreationTimestamp private Date createdAt;
+	 * 
+	 * @CreationTimestamp private Date updatedAt;
+	 */
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Product;
+
+public interface ProductService {
+	
+	Product createProduct(Product product);
+	Product updateProduct(Product product);
+	
+
+	List<Product> getAllProduct();
+	
+	Product getProductById(long productId);
+
+	void deleteProduct(long id);
+}
